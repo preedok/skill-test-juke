eda3ac2f69d69e43f836fbc8014d8b5a21eb19c1
preedok <edo.vivoy21@gmail.com>
2024-09-11 12:14:09 +0700

    fix : add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import HomePage from "./components/HomePage"
-import PostPage from "./components/PostPage"
+import React from 'react';
+import AppRouter from './router/AppRouter';
+
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" Component={HomePage}/>
-          <Route path="/posts/:id" Component={PostPage}/>
-        </Routes>
-      </div>
-    </Router>
-  )
-}
+    <AppRouter />
+  );
+};
 
-export default App
+export default App;
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+// src/AppRouter.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from '../components/HomePage';
+import PostPage from '../components/PostPage';
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/posts/:id" element={<PostPage />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
+
+export default AppRouter;
